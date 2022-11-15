57ec149636062205f1e1234ce3f8b20e6475c757
Denis Kulik <kulikdenis1996x@gmail.com>
2022-11-15 17:10:25 +0700

    add the possibility of user authorization

---
+++ public/js/ui/forms/LoginForm.js
@@ -0,0 +1,13 @@
+class LoginForm extends AsyncForm {
+  onSubmit(data) {
+    User.login(data, (err, response) => {
+      if (response && response.success) {
+        this.element.reset();
+        App.setState('user-logged');
+        App.getModal('login').close();
+      } else {
+        alert(JSON.stringify(response.error));
+      }
+    });
+  }
+}
