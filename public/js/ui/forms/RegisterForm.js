df257a650b3838826014c6882a061a29203ea71e
Denis Kulik <kulikdenis1996x@gmail.com>
2022-11-15 17:08:43 +0700

    add the possibility of user registration

---
+++ public/js/ui/forms/RegisterForm.js
@@ -0,0 +1,13 @@
+class RegisterForm extends AsyncForm {
+  onSubmit(data) {
+    User.register(data, (err, response) => {
+      if (response && response.success) {
+        this.element.reset();
+        App.setState('user-logged');
+        App.getModal('register').close();
+      } else {
+        alert(JSON.stringify(response.error));
+      }
+    });
+  }
+}
